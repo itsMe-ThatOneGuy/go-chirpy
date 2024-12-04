35bfcab0fa2f30de608d66ed3f2f37be78c59e1e
Matthew Smart <matthewsmart13@gmail.com>
2024-12-04 17:11:06 -0600

    Created basic server

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+
+	mux := http.NewServeMux()
+	server := http.Server{
+		Handler: mux,
+		Addr:    ":8080",
+	}
+
+	server.ListenAndServe()
+
+}
