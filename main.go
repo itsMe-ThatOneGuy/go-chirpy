f1d4a198ff55c40fd47a67df43d8fc5273e12b95
Matthew Smart <matthewsmart13@gmail.com>
2024-12-05 11:39:28 -0600

    Added a heal handler

--- main.go
+++ main.go
@@ -11,6 +11,11 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.Handle("/", http.FileServer(http.Dir(root)))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	})
 
 	server := &http.Server{
 		Handler: mux,
