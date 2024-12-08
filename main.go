ef593007245cf549d15552f751573804a10f2f42
Matthew Smart <matthewsmart13@gmail.com>
2024-12-07 23:13:08 -0600

    helper funcs for json responses

--- main.go
+++ main.go
@@ -72,3 +72,34 @@ func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
 
 	w.Write([]byte(msg))
 }
+func responseError(w http.ResponseWriter, status int, msg string, err error) {
+	if err != nil {
+		log.Println(err)
+	}
+
+	if status > 499 {
+		log.Printf("Responding with 5XX error: %s", msg)
+	}
+
+	type jsonResError struct {
+		Error string `json:"error"`
+	}
+
+	jsonResponse(w, status, jsonResError{
+		Error: msg,
+	})
+}
+
+func jsonResponse(w http.ResponseWriter, status int, payload interface{}) {
+	res, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(status)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(res)
+
+}
