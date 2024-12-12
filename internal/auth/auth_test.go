0d0567be6d33d54a2badfaa278a9d8e602edefde
Matthew Smart <matthewsmart13@gmail.com>
2024-12-12 00:51:08 -0600

    MakeJWT test

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,6 +1,12 @@
 package auth
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/google/uuid"
+)
 
 func TestHashed(t *testing.T) {
 	password := "testingpassword"
@@ -29,4 +35,17 @@ func TestCheckPasswordHash(t *testing.T) {
 	if err == nil {
 		t.Error("Password expect to not match, but did match")
 	}
+}
+
+func TestMakeJWT(t *testing.T) {
+	userID, _ := uuid.Parse("a0de6ac2-0b73-432c-a5db-b96d6451251f")
+	tokenSecret := "test"
+	expiresIn := time.Duration.Seconds(30)
+
+	jwt, err := MakeJWT(userID, tokenSecret, time.Duration(expiresIn))
+	if err != nil {
+		t.Errorf("MakeJWT returned a non nil error: %v", err)
+	}
+
+	fmt.Println(jwt)
 }
