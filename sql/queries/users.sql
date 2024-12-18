51eea799c628139614d81665219f2c1249987707
Matthew Smart <matthewsmart13@gmail.com>
2024-12-18 14:07:50 -0600

    upgrade user query

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -21,3 +21,8 @@ UPDATE users
 SET email = $1, hashed_password = $2, updated_at = NOW()
 WHERE id = $3
 RETURNING *;
+
+-- name: UpgradeUser :exec
+UPDATE users
+set is_chirpy_red = TRUE, updated_at = NOW()
+WHERE id = $1;
